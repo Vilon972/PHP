c394096b1da79b2c1534dc876ba1d83e72fe667b
Théa Manette <Thea@MacBook-Pro-de-Thea-MANETTE.local>
2019-01-08 18:56:16 +0100

    good

---
+++ Site_ecommerce/conditions_generales_de_vente.php
@@ -0,0 +1,20 @@
+<?php 
+
+require_once('includes/header.php');
+require_once('includes/sidebar.php');
+
+?>
+
+<br/><h1>Conditions générales de vente</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br/>
+
+<?php
+require_once('includes/footer.php');
+
+?>
---
+++ Site_ecommerce/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+<h5>Copyright &copy; Site E-Commerce 2013</h5>
+</div>
